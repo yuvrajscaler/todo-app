ac2eaec1a83e3809bee5183d946cbc740f0e41c2
Yuvraj singh <yuvrajthakur292@gmail.com>
2022-01-04 15:27:02 +0530

    added mark as complete functionality

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,11 +1,12 @@
 class TodoItemsController < ApplicationController
  before_action :set_todo_list
+ before_action :set_todo_item, except: [:create]
 
  def create
     @todo_item = @todo_list.todo_items.create(todo_items_params)
     redirect_to @todo_list
  end
- 
+
  def destroy
     @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.destroy
@@ -15,11 +16,20 @@ class TodoItemsController < ApplicationController
     end
     redirect_to @todo_list 
    end
+ def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item completed"
+   end
  private
 
  def set_todo_list
     @todo_list= TodoList.find(params[:todo_list_id])
  end
+
+ def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
+   end
+
 
  def todo_items_params
     params[:todo_item].permit(:content)
